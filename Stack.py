c4df48184706bdecb54980baae226c8ec545469b
Deadlynight0 <byrayael@gmail.com>
2024-04-17 19:19:40 +0300

    feat(project:Stack) add node stack

---
+++ Stack.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self):
+        pass
+
+
+class Stack:
+    def __init__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
