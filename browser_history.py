beb799fe51b77c3343db7b673c37b049a070aaad
diamolezo <diamolezo@gmail.com>
2024-04-17 21:40:16 +0300

    fix(project:main) Browser history

--- browser_history.py
+++ browser_history.py
@@ -1,3 +1,5 @@
+import datetime
+
 import Stack as s
 class BrowserHistory:
 
@@ -7,21 +9,42 @@ class BrowserHistory:
         """
 
         self.__history_stack = s.Stack()
+        self.__history_stack_active = s.Stack()
         self.__current_index = 0
+
+    class HistoryItem:
+
+        def __init__(self, url, datetime):
+            self.url = url
+            self.datetime = datetime
 
     def visit(self, url: str):
         """
         Создаёт объект с текущим временем и добавляет его в стэк
         :return: None
         """
+        import datetime
+        item = BrowserHistory.HistoryItem(url, datetime.datetime.now())
+        self.__history_stack.push(item)
+        self.__current_index += 1
+
 
     def back(self) -> object:
         """
-        Возвращает предыдущий оюъект из истории
+        Возвращает предыдущий объект из истории
         :return: Объект
         """
         if self.__current_index == 0:
             return -1
+
+
+        buff1 = self.__history_stack.pop()
+        self.__history_stack_active.push(buff1)
+
+        return buff1
+
+
+
 
 
     def forward(self) -> object:
@@ -29,13 +52,18 @@ class BrowserHistory:
         Возвращает следующий объект в истории
         :return: объект
         """
-        pass
+        buff = self.__history_stack_active.pop()
+        self.__history_stack.push(buff)
+
+        return buff
+
+
 
     def clear(self) -> None:
-        """
-        Очищает историю просмотра страниц
-        """
         pass
+
+
+
 
     def all(self) -> str:
         """
