2b78590f6bad222ac6b3f010aff477cfbc044ab4
Deadlynight0 <byrayael@gmail.com>
2024-04-17 21:05:52 +0300

    feat(project:stacktest) add tests

--- stacktests.py
+++ stacktests.py
@@ -15,6 +15,20 @@ class StackPushTestCase(unittest.TestCase):
         res = self.stack.peek()
         self.assertEquals(res, -1121423)
 
+    def test_push_zero_int(self):
+        self.stack.push(0)
+        res = self.stack.peek()
+        self.assertEquals(res, 0)
+
+    def test_push_data_type_str(self):
+        self.stack.push('-1121423sfjisjdfsidfuygadsad')
+        res = self.stack.peek()
+        self.assertEquals(res, '-1121423sfjisjdfsidfuygadsad')
+
+    def test_push_data_type_int(self):
+        self.stack.push(0.6)
+        res = self.stack.peek()
+        self.assertEquals(res, 0.6)
 
 if __name__ == '__main__':
     unittest.main()
