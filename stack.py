cccc4ed6c3c2ae77a1b3eb0730ede49d67e47e2c
ZeQipe <zeqipe@vk.com>
2024-04-17 19:41:34 +0300

    feat(project:stack) add method

--- stack.py
+++ stack.py
@@ -1,2 +1,12 @@
 class Stack:
-    pass 
+    def __init__(self):
+        pass
+
+    def add(self, item: any):
+        pass
+
+    def last_del(self):
+        pass
+
+    def get_last(self):
+        pass
