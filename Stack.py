97782201a569e26671177ae632885ac68e2051cc
Deadlynight0 <byrayael@gmail.com>
2024-04-17 20:30:38 +0300

    feat(project:Stack) add Annotation:

--- Stack.py
+++ Stack.py
@@ -9,13 +9,22 @@ class Stack:
         self.__top = None
         self.__size = 0
 
-    def push(self,item):
+    def push(self, item: any) -> None:
+        """
+        Данная функция добавляет ,элемент любого типа данных в класс
+        :param item: объект любого типа данных
+        :return: None
+        """
         node = Node(item)
         node.prev = self.__top
         self.__top = node
         self.__size += 1
 
-    def pop(self):
+    def pop(self) -> None or any:
+        """
+        Данная функция удаляет ,верхний элемент класса
+        :return: Удаленный элемент, любого типа данных который находился на вверхушке
+        """
         if self.__size == 0: return None
 
         data = self.__top.data
@@ -24,13 +33,21 @@ class Stack:
 
         return data
 
-    def peek(self):
+    def peek(self) -> None or any:
+        """
+        Данная функция показывает,элемент который находится на вверху
+        :return: None либо элемент ,любого типа данных
+        """
         if self.__size != 0:
             return self.__top.data
         else:
             return
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
+        """
+        Данная функция проверяет класс на пустоту
+        :return:True если элемента в классе нет,False если есть
+        """
         return self.__top == None
 
 
